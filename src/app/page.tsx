82a49d1928bbea586ac4e451bbd5061555a0231e
conejofoz <conejofoz@gmail.com>
2025-01-03 12:16:41 -0300

    usando async await

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,23 @@ const Page = ()=>{
   const [loading, setLoading] = useState(true);
   const [users, setUsers] = useState<User[]>([]);
 
+  const getUsers = async ()=>{
+    setLoading(true);
+    try {
+      const resposta = await fetch('https://jsonplaceholder.typicode.com/users');
+      const json = await resposta.json()
+      setUsers(json);
+    } catch (error) {
+      console.log("DEU ALGO ERRADO ", error)
+    }
+    setLoading(false);
+  }
+
   useEffect(()=>{
+    getUsers(); //segundo o professor useEffect não aceita async
+  }, []);
+
+  /* useEffect(()=>{
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
     .then(json => {
@@ -19,7 +35,8 @@ const Page = ()=>{
     .finally(()=>{
       setLoading(false);
     });
-  }, [])
+  }, []) */
+
   return (
     <div className="container mx-auto">
       <h1 className="text-3xl">Lista de usuários</h1>
